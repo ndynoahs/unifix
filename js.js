0a409748da6a3468f62fb3607fcb5f19ba6428c9
ndynoahs <ndynoahs@gmail.com>
2021-05-31 15:21:46 +0100

    new update

---
+++ js.js
@@ -0,0 +1,21 @@
+
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: true,
+//     responsive: {
+//         0:{
+//            items: 1 
+//         }
+//     }
+// })
+    
+
+
+
+
+// VanillaTilt.init(document.querySelectorAll(".register-form"), {
+// max: 25,
+// speed: 400
+// });
+
